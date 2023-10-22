667462d8df4e971b5c2bcc6cc6361c5561fcf5fb
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2023-10-23 06:17:52 +0700

    refactoring app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./pages/home";
 import Products from "./pages/products";
 import ErrorPage from "./pages/error";
+import SingleProduct from "./pages/detail-products";
 
 const App = () => {
   return (
@@ -10,9 +10,11 @@ const App = () => {
       <Routes>
         <Route element={<Home />} path="/" />
         <Route element={<Products />} path="/products" />
+        <Route element={<SingleProduct />} path="/products/:id" />
         <Route element={<ErrorPage />} path="*" />
       </Routes>
     </BrowserRouter>
   );
 };
+
 export default App;
