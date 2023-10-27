f797c5260350000967ddcc2383cc43b469f1c62d
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2023-10-27 13:27:14 +0700

    fixing cart layout

--- src/pages/products/Cart.jsx
+++ src/pages/products/Cart.jsx
@@ -52,17 +52,17 @@ export default function Cart({ showModal, toggle }) {
       <div className="flex-col flex items-center fixed inset-0 left-0 right-0 overflow-y-auto w-full bg-white dark:bg-black gap-8 p-10 text-black dark:text-white font-normal uppercase text-sm">
         <ToastContainer />
         <h1 className="text-2xl font-bold">Cart</h1>
-        <div className="absolute right-16 top-10">
+        <div className="absolute right-16 top-10 lg:right-28">
           <button
-            className="px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
+            className="px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700 "
             onClick={toggle}
           >
             Close
           </button>
         </div>
         {cartItems.length > 0 && (
-          <table className="min-w-full">
-            <thead className="border-b">
+          <table className="container ">
+            <thead>
               <tr>
                 <th className="py-2 text-start">Item</th>
                 <th className="py-2 text-start">Price</th>
@@ -74,7 +74,7 @@ export default function Cart({ showModal, toggle }) {
               {cartItems.map((item) => (
                 <tr key={item.id}>
                   <td className="py-2">
-                    <div className="flex items-center">
+                    <div className="md:flex md:items-center">
                       <img
                         src={item.image}
                         alt={item.name}
@@ -85,9 +85,9 @@ export default function Cart({ showModal, toggle }) {
                       </div>
                     </div>
                   </td>
-                  <td className="py-2">${item.price}</td>
+                  <td className="py-2  mr-2">${item.price}</td>
                   <td className="py-2">
-                    <div className="flex items-center">
+                    <div className="flex items-center md:flex md:items-center">
                       <button
                         className="px-2 py-1 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
                         onClick={() => {
@@ -96,7 +96,7 @@ export default function Cart({ showModal, toggle }) {
                       >
                         +
                       </button>
-                      <p className="px-4">{item.quantity}</p>
+                      <p className="px:2 md:px-4">{item.quantity}</p>
                       <button
                         className={`px-2 py-1 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700 ${
                           item.quantity === 1
@@ -134,20 +134,30 @@ export default function Cart({ showModal, toggle }) {
           </table>
         )}
         {cartItems.length > 0 ? (
-          <div className="flex flex-col justify-between items-center">
-            <h1 className="text-lg font-bold">Total: ${getCartTotal()}</h1>
+          <div className=" flex flex-col justify-end items-end container">
             <button
-              className="px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
+              className="px-4 py-3 bg-gray-800 text-white text-xs font-bold uppercase rounded-[2px] hover:bg-gray-700 w-max focus:outline-none focus:bg-gray-700"
               onClick={() => {
                 clearCart();
                 notifyCartCleared();
               }}
             >
-              Clear cart
+              Clear Shopping Cart
             </button>
+            {/* <h1 className="text-lg font-bold">Total: ${getCartTotal()}</h1> */}
           </div>
         ) : (
           <h1 className="text-lg font-bold">Your cart is empty</h1>
+        )}
+        {cartItems.length > 0 && (
+          <div className="container flex justify-end">
+            <div className=" text-end">
+              <h1 className="text-lg font-bold ">Total: ${getCartTotal()}</h1>
+              <button className="bg-[#286F6C] text-white py-2 px-12 rounded">
+                Checkout
+              </button>
+            </div>
+          </div>
         )}
       </div>
     )
